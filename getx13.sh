a7f3f60d680c031d02e59ad351e3ae150287f3f2
Chris Hansen <cmhhansen@outlook.com>
2020-11-25 23:57:37 +1300

    Update getx13.sh

--- getx13.sh
+++ getx13.sh
@@ -1,6 +1,6 @@
 mkdir -p /tmp/x13
 mkdir -p ${HOME}/.local/bin
-cd /tmp/x13 && \
+cd /tmp/x13 
 wget https://www.census.gov/ts/x13as/unix/x13ashtmlall_V1.1_B39.tar.gz 
 tar -xvf x13ashtmlall_V1.1_B39.tar.gz 
 mv x13ashtml ${HOME}/.local/bin
