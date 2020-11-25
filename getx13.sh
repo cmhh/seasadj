447c04279ed6775187943962815b4bd437459930
Chris Hansen <cmhhansen@outlook.com>
2020-11-25 23:57:16 +1300

    Update getx13.sh

--- getx13.sh
+++ getx13.sh
@@ -5,4 +5,3 @@ wget https://www.census.gov/ts/x13as/unix/x13ashtmlall_V1.1_B39.tar.gz
 tar -xvf x13ashtmlall_V1.1_B39.tar.gz 
 mv x13ashtml ${HOME}/.local/bin
 echo "export PATH=$PATH:$HOME/.local/bin" >> ${HOME}/.bashrc
-source ${HOME}/.bashrc
