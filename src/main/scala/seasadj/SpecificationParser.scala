fa3ede60c72033644c5ab1d7d295cc9066776ef0
Chris Hansen <cmhhansen@outlook.com>
2021-01-14 11:32:51 +1300

    Added docstrings.

--- src/main/scala/seasadj/SpecificationParser.scala
+++ src/main/scala/seasadj/SpecificationParser.scala
@@ -3,6 +3,9 @@ package org.cmhh.seasadj
 import org.cmhh.seasadj._
 import scala.util.{Try, Success, Failure}
 
+/**
+ * Static object used to parse SPC files.
+ */
 object SpecificationParser {
   // linebreak
   val linebreak = "(?:(?:\r\n)|\n)"
@@ -23,23 +26,53 @@ object SpecificationParser {
   // high-level match for entire spec 'file', i.e. name { param = value}\nname { param = value}...
   val specifications = s"^( *${linebreak}? *${specification} *${linebreak}? *)+$$"
 
+  /**
+    * Make a single regex which matches 'any of' several others.
+    *
+    * @param rs list of regular expression strings
+    * @return A regular expression string
+    */
   def anyOf(rs: Seq[String]): String = 
     "(" + rs.map(r => s"(?:$r)").mkString("|") + ")"
 
+  /**
+    * Remove comments from a specification.
+    * 
+    * Comments are denoted by #.  Remove any lines that start with #, or remove
+    * content after # if it appears part-way through a line.
+    *
+    * @param str the body of a SPC file
+    * @return the body of a SPC file with comments removed.
+    */
   def stripComment(str: String): String = {
     if (str.startsWith("#")) ""
     else if (str.contains("#")) str.take(str.indexOf("#"))
     else str
   }
 
+  /**
+    * Standardise newlines, remove comments, and trim some whitespace from the
+    * content of a SPC file.
+    *
+    * @param str the body of a SPC file
+    * @return tidier version of the body of a SPC file
+    */
   def preprocess(str: String): String = 
     str
       .split("\\R")
-      .map(stripComment)
+      .map(stripComment) 
       .map(_.trim)
       .filter(line => line != "")
       .mkString("\n")
 
+  /**
+    * Split body of SPC files into individual specs.
+    * 
+    * Extract instances of the pattern `spec { stuff }`, where spec is, for example, {{series}}, {{x11}}, etc.
+    *
+    * @param str
+    * @return
+    */
   def parseSpecs(str: String): Try[Seq[String]] = Try { 
     val processed = preprocess(str)
     if (!processed.matches(specifications)) 
@@ -47,6 +80,14 @@ object SpecificationParser {
     specification.r.findAllIn(processed).toList
   }
 
+  /**
+    * Split the body of specifications into name-value pairs.
+    * 
+    * Given text with pattern `spec { stuff }`, convert to {{(spec, stuff)}}.
+    *
+    * @param str specification text
+    * @return key-value pair
+    */
   def parseSpecNameAndBody(str: String): Try[(String, String)] = {
     try {
       val r = s"^($name)${lbrace}((?:$ws)|(?:$anytext))${rbrace}$$".r
@@ -59,7 +100,17 @@ object SpecificationParser {
     }
   }
   
+  /**
+    * Split body of spec into name-value pairs.
+    * 
+    * Given `stuff` extracted from `spec { stuff }`, convert `stuff` to 
+    * {{(key, value)}}.  For example, {{("sigamlim", "(1.8, 2.8)")}}
+    *
+    * @param str input text, e.g. "sigmalim=(1.8,2.8)"
+    * @return [[Try[Map[String, String]]]]
+    */
   def parseArgs(str: String): Try[Map[String, String]] = Try {
+    // when '(' is detected, find the remaining sequence up to ')'
     def complete(s: String, numLeft: Int, numRight: Int, accum: String): (String, String) = {
       if (s == "") throw new IllegalArgumentException(s"""Invalid specification value: "${str}".""")
       else if (s.take(1) == "(") complete(s.drop(1), numLeft + 1, numRight, accum + s.take(1))
@@ -92,6 +143,12 @@ object SpecificationParser {
     parse(str, Seq.empty).toMap
   }
 
+  /**
+    * Convert body of a SPC file into a [[Specs]] object.
+    *
+    * @param str
+    * @return
+    */
   def parse(str: String): Try[Map[String, Map[String, SpecValue]]] = Try {
     val res = parseSpecs(str) match {
       case Success(specs) => 
