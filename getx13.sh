18c1465e2eae3c7e5ddc37d9902bbab2314805c0
cmhh <cmhhansen@outlook.com>
2021-07-10 23:37:31 +0000

    removed script

--- getx13.sh
+++
@@ -1,7 +0,0 @@
-mkdir -p /tmp/x13
-mkdir -p ${HOME}/.local/bin
-cd /tmp/x13 
-wget https://www.census.gov/ts/x13as/unix/x13ashtmlall_V1.1_B39.tar.gz 
-tar -xvf x13ashtmlall_V1.1_B39.tar.gz 
-mv x13ashtml ${HOME}/.local/bin
-echo "export PATH=$PATH:$HOME/.local/bin" >> ${HOME}/.bashrc
